496cf6022c2e57fa91a57b7ffcc7d8af7086ee80
toleman84 <gdponcemarsiglia@gmail.com>
2024-09-22 19:22:52 -0300

    fix files

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,14 +6,14 @@ def minOperations(n):
     """min operations"""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
 
     while n > 1:
         while n % divisor == 0:
-            operations += divisor # H => Copy All => Paste => HH
-            n //= divisor # operador de asignación de división entera
+            operations += divisor  # H => Copy All => Paste => HH
+            n //= divisor  # operador de asignación de división entera
         divisor += 1
-    
+
     return operations
